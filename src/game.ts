8c706de899c2390643831311be041ae83ff0b3b4
nemo <n3m0o7@hotmail.com>
2026-04-20 23:06:12 +0200

    salle

--- src/game.ts
+++ src/game.ts
@@ -1,2 +1,5 @@
 import inventaire from "./inventaire.ts"
 window.inventaire = inventaire;
+
+import salle from "./salle.ts"
+window.salle = salle;
---
+++ src/salle.ts
@@ -0,0 +1,39 @@
+import salles from "./data/salles.json"
+
+let salle_in = "morgue";
+
+let onchange_callback = () => {};
+
+function get_current() {
+    return salles[salle_in];
+}
+
+function get_link(id) {
+    let salle = salles[salle_in];
+    if (id == 0 || id == 1) return salles[salle.links[id]];
+
+    let links = [
+        salles[salle.links[0]],
+        salles[salle.links[1]],
+    ];
+    return links;
+}
+
+function salle_go(id) {
+    if (!(id == 0 || id == 1)) return false;
+
+    salle_in = salles[salle_in].links[id];
+
+    onchange_callback();
+}
+
+function set_onchange_callback(cb: (() => void)) {
+    onchange_callback = cb;
+}
+
+export default {
+    current: get_current,
+    link: get_link,
+    go: salle_go,
+    onchange: set_onchange_callback,
+}
