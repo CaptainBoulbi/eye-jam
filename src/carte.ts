242dc7bf5dddcc6697304d0d7d50981eb312c46a
nemo <n3m0o7@hotmail.com>
2026-04-22 21:32:11 +0200

    algo carte presque complet

--- src/carte.ts
+++ src/carte.ts
@@ -4,9 +4,67 @@ import salle from "./salle.ts"
 import stat from "./stat.ts"
 import inventaire from "./inventaire.ts"
 
+let no_carte_left_loser = {
+    id: "no_carte_left_loser",
+    name: "Nothing to see, take that candy",
+    content: "You can't do much, exept staring at the ceiling, and swallow that candy",
+    reference: null,
+    salle: ["morgue", "rest_room", "lab", "office", "isolation"],
+    stats: {
+        sante: 0,
+        lucide: 0,
+        stress: 0
+    },
+    deal: [
+        {
+            sante: 2,
+            lucide: 1,
+            stress: 1
+        },
+        {
+            sante: 1,
+            lucide: 1,
+            stress: 2
+        }
+    ],
+    ingredients: [],
+    craft: [null, null],
+    consume: [null, null],
+    image: "assets/innexistant.png"
+};
+
+let first_carte = {
+    id: "first",
+    name: "Welcome to the lab",
+    content: "Welcome to the lab",
+    reference: null,
+    salle: ["morgue", "rest_room", "lab", "office", "isolation"],
+    stats: {
+        sante: 0,
+        lucide: 0,
+        stress: 0
+    },
+    deal: [
+        {
+            sante: 0,
+            lucide: 0,
+            stress: 0
+        },
+        {
+            sante: 0,
+            lucide: 0,
+            stress: 0
+        }
+    ],
+    ingredients: [],
+    craft: [null, null],
+    consume: ["log_d1", "log_d45"],
+    image: "assets/innexistant.png"
+};
+
 let onchange_callback = () => {};
 
-let carte_actuel = get_carte(null);
+let carte_actuel = get_carte(first_carte.id);
 let used_carte = [];
 
 function set_onchange_callback(cb: (() => void)) {
@@ -23,19 +81,17 @@ function next_carte(choice) {
     stat.types().forEach((type) => {
         stat.add(type, carte_actuel.deal[choice][type]);
     })
-    inventaire.add(carte_actuel.craft);
+    inventaire.add(carte_actuel.craft[choice]);
+    inventaire.remove(carte_actuel.consume[choice]);
 
     let access = get_accessible_carte();
-    // if (access.length <= 0) {
-    //     used_carte = [];
-    //     access = get_accessible_carte();
-    //     if (access.length <= 0) {
-    //         // TODO: sa veut dire que aucune carde n'est possible avec les stats actuel
-    //         //       normallement on est senser avoir des carte possible avec n'importe quelle stat
-    //         //       mais si au case ou on devrait retourner un carte special qui donne +1 a toute les stats
-    //         //       ou un truc dans le genre
-    //     }
-    // }
+    if (access.length <= 0) {
+        used_carte = [];
+        access = get_accessible_carte();
+        if (access.length <= 0) {
+            access.push(no_carte_left_loser.id);
+        }
+    }
     let carte_id = access[parseInt(Math.random() * access.length)];
     carte_actuel = get_carte(carte_id);
     used_carte.push(carte_id);
@@ -50,7 +106,7 @@ function get_accessible_carte() {
     Object.keys(cartes).forEach((carte_id) => {
         let carte = cartes[carte_id];
 
-        if (carte.salle != salle.current().id) return;
+        if (carte.salle.indexOf(salle.current().id) < 0) return;
 
         let nb_stat_satisfied = 0;
         stat.types().forEach((type) => {
@@ -75,6 +131,8 @@ function get_accessible_carte() {
 }
 
 function get_carte(id) {
+    if (id == no_carte_left_loser.id) return no_carte_left_loser;
+    if (id == first_carte.id) return first_carte;
     if (!id || !cartes[id]) {
         let empty_carte = {}
         Object.getOwnPropertyNames(cartes[Object.keys(cartes)[0]]).forEach((cid) => {
