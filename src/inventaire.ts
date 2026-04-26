d507b1357d6f00452e04e8e65f3105dacfc141a1
nemo <n3m0o7@hotmail.com>
2026-04-26 18:12:43 +0200

    je m'excuse aupres d'ebral, j'ai refait tout ton css

--- src/inventaire.ts
+++ src/inventaire.ts
@@ -98,24 +98,24 @@ function set_onchange_callback(cb: (() => void)) {
 
 function test_data() {
     // check if grid empty
-    for (let i = 0; i < grid_cap; i++) {
-        if (grid[i]) return;
-    }
+    // for (let i = 0; i < grid_cap; i++) {
+    //     if (grid[i]) return;
+    // }
 
-    for (let i = 0; i < 3; i++) {
-        let items_idx = parseInt(Math.random() * item.keys().length);
-        let coord = null;
-        let max = 50;
-        do {
-            coord = {
-                x: Math.floor(Math.random() * grid_size.x),
-                y: Math.floor(Math.random() * grid_size.y),
-            };
-            max--
-        } while (get_item(coord) && max > 0);
+    // for (let i = 0; i < 3; i++) {
+    //     let items_idx = parseInt(Math.random() * item.keys().length);
+    //     let coord = null;
+    //     let max = 50;
+    //     do {
+    //         coord = {
+    //             x: Math.floor(Math.random() * grid_size.x),
+    //             y: Math.floor(Math.random() * grid_size.y),
+    //         };
+    //         max--
+    //     } while (get_item(coord) && max > 0);
 
-        let ret = set_item(coord, item.keys()[items_idx]);
-    };
+    //     let ret = set_item(coord, item.keys()[items_idx]);
+    // };
 
     onchange_callback();
 }
